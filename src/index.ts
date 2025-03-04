60c991f4fc309d339ba3bfca07165f6834e7b39d
Guilherme China <echina725@gmail.com>
2025-03-04 16:32:56 -0300

    interval based chrons

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,59 @@
-import { sumStruct } from "./types";
-
-function sum(sumStruct: sumStruct) {
-  return sumStruct.a + sumStruct.b;
-}
-
-export {
-  sum,
-}
+import { ExecFrequency, IntervalTask } from "./types";
 
 export * from "./types"
+
+class TaskManager {
+  private intervalRunningTasks: Array<IntervalTask>
+  
+  constructor() {
+    this.intervalRunningTasks = [];
+  }
+
+  public makeTask(): IntervalTask {
+    return new IntervalTask();
+    
+  }
+
+  protected addIntervalTask(task: IntervalTask) {
+    this.intervalRunningTasks.push(task);
+  }
+
+  protected getTask(id: string) {
+    return this.intervalRunningTasks.find((t) => t.id === id);
+  }
+}
+
+export class Chronos extends TaskManager {
+  
+  constructor() {
+    super();
+  }
+
+  public execEvery(freq: number | ExecFrequency, handler: () => void, times?: number) {
+    let ofcHandler = handler;
+    const task = this.makeTask(); 
+    this.addIntervalTask(task);
+
+    if(freq < 0) {
+      console.error("frequency interval must be greater than 0");
+      return;
+    }
+
+    if(times) {
+      ofcHandler = async () => {
+        if(task.getExecTimes() <= times) {
+          await handler();
+          task.incrExecTimes();
+          return;
+        }
+      }
+    }
+
+    setInterval(ofcHandler, freq);
+  }
+}
+
+/* chronos.execEvery(5000, () => { */
+/*   console.log("Executing chron"); */
+/* }) */
+
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,29 @@
-export type sumStruct = {
-  a: number;
-  b: number;
+export class IntervalTask {
+  public readonly id: string;
+  private execTimes: number;
+  public readonly intervalId: number;
+
+  constructor(id?: string) {
+    if(!id) {
+      this.id = Date.now().toString();
+    } else {
+      this.id = id;
+    }
+
+    this.execTimes = 0;
+    this.intervalId = 0;
+  }
+
+  public incrExecTimes() {
+    this.execTimes++;
+  }
+
+  public getExecTimes() {
+    return this.execTimes;
+  }
 }
+
+export enum ExecFrequency {
+  EVERY_5_MINUTES = 5 * 60 * 1000
+}
+
